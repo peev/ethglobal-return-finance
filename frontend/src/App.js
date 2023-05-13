7f032bbcb81fd0d4f76eb70af416b493dfb3188d
peev <christo.peev@gmail.com>
2023-05-13 13:11:36 +0300

    Add MM event listeners and updateBalance function

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -31,7 +31,67 @@ provider.getSigner()
 
 function App() {
   const [walletBalance, setWalletBalance] = useState(0);
-  const [faucetBalance, setFaucetBalance] = useState(0);
+  const [buttonText, setButtonText] = useState("Connect");
+  const [buttonDisabled, setButtonDisabled] = useState(false);
+  const [onRightNetwork, setOnRightNetwork] = useState(false);
+  
+  // array populated by MetaMask onConnect
+  const [accounts, setAccounts] = useState([]);
+  const [isConnected, setIsConnected] = useState(
+    accounts && accounts.length > 0
+  );
+
+  // MetaMask event listeners
+  ethereum.on("chainChanged", handleNewNetwork);
+  ethereum.on("accountsChanged", (newAccounts) => {
+    handleNewAccounts(newAccounts);
+    updateBalances();
+  });
+
+    // any time accounts changes, toggle state in the Connect button
+    useEffect(() => {
+      setIsConnected(accounts && accounts.length > 0);
+    }, [accounts]);
+  
+    useEffect(() => {
+      if (isConnected) {
+        setButtonText("Connected");
+        setButtonDisabled(true);
+      } else {
+        setButtonText("Connect");
+        setButtonDisabled(false);
+      }
+    }, [isConnected]);
+
+  // protect users from using the wrong network
+  // disables the withdraw/deposit buttons if not on Optimism
+  async function handleNewNetwork() {
+    const chainId = await ethereum.request({
+      method: "eth_chainId",
+    });
+    console.log(chainId);
+    if (chainId != 10) {
+      setOnRightNetwork(false);
+    } else {
+      setOnRightNetwork(true);
+    }
+  }
+
+  // uses MM SDK to query latest Faucet and user wallet balance
+  async function updateBalances() {
+    if (accounts.length > 0) {
+      const faucetBalance = await ethereum.request({
+        method: "eth_getBalance",
+        params: [RETURN_CONTRACT_ADDRESS, "latest"],
+      });
+      const walletBalance = await ethereum.request({
+        method: "eth_getBalance",
+        params: [accounts[0], "latest"],
+      });
+      setFaucetBalance(Number(faucetBalance) / 10 ** 18);
+      setWalletBalance(Number(walletBalance) / 10 ** 18);
+    }
+  }
   return (
     <div className="container">
       <img className="header-image" alt="" src="https://4033360328-files.gitbook.io/~/files/v0/b/gitbook-x-prod.appspot.com/o/spaces%2FCn5NyIfWAJDviT4SySgQ%2Fuploads%2FioJYt2OT0YM59pv5WasM%2FScreenshot%202023-01-10%20at%2022.26.10.png?alt=media&amp;token=142c82ac-855c-4fbf-8a07-e7de37d9eade"></img>
@@ -43,7 +103,7 @@ function App() {
         </span>
       </h1>
       <div className="balance-container">
-        <h2 className="balance-item">Circle Balance: {faucetBalance} USDC</h2>
+        <h2 className="balance-item">Circle Balance: 10k+ USDC</h2>
         <h2 className="balance-item">My Wallet Balance: {walletBalance} USDC</h2>
       </div>
       <div className="button-container">
