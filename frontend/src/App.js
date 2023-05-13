0f92a34cd5f7479dce7b7dd43eee6511c420a2d0
peev <christo.peev@gmail.com>
2023-05-13 14:12:11 +0300

    Fix network switching via MM sdk

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import MetaMaskSDK from "@metamask/sdk";
 import { ethers } from "ethers";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 
 // MetaMask SDK initialization
@@ -8,7 +8,7 @@ const MMSDK = new MetaMaskSDK();
 const ethereum = MMSDK.getProvider();
 
 // This app will only work on Optimism
-const OPTIMISM_CHAIN_ID = "10";
+const OPTIMISM_CHAIN_ID = "0xa";
 
 // Return Finance contract initialization
 const provider = new ethers.providers.Web3Provider(ethereum, "any");
@@ -54,6 +54,7 @@ function App() {
     }, [accounts]);
   
     useEffect(() => {
+    
       if (isConnected) {
         setButtonText("Connected");
         setButtonDisabled(true);
@@ -88,10 +89,89 @@ function App() {
         method: "eth_getBalance",
         params: [accounts[0], "latest"],
       });
-      setFaucetBalance(Number(faucetBalance) / 10 ** 18);
       setWalletBalance(Number(walletBalance) / 10 ** 18);
     }
   }
+
+    // handles user connecting to MetaMask
+  // will add Optimims network if not already a network in MM
+  // will switch to Optimism (bug, can't currently do this)
+  async function onClickConnect() {
+    try {
+      const newAccounts = await ethereum.request({
+        method: "eth_requestAccounts",
+      });
+      handleNewAccounts(newAccounts);
+      try {
+        await window.ethereum.request({
+          method: "wallet_switchEthereumChain",
+          params: [{ chainId: OPTIMISM_CHAIN_ID }], // Check networks.js for hexadecimal network ids
+        });
+      } catch (e) {
+        if (e.code === 4902) {
+          try {
+            await window.ethereum.request({
+              method: "wallet_addEthereumChain",
+              params: [
+                {
+                  chainId: OPTIMISM_CHAIN_ID,
+                  chainName: "Optimism",
+                  rpcUrls: [
+                    "https://rpc.ankr.com/optimism",
+                  ],
+                  nativeCurrency: {
+                    name: "Optimism",
+                    symbol: "OP",
+                    decimals: 18,
+                  },
+                  blockExplorerUrls: ["https://optimistic.etherscan.io/"],
+                },
+              ],
+            });
+          } catch (e) {
+            console.log(e);
+          }
+        }
+      }
+      setIsConnected(true);
+      handleNewNetwork();
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  // hook to change state
+  function handleNewAccounts(newAccounts) {
+    setAccounts(newAccounts);
+  }
+
+  // withdraws .01 ETH from the faucet
+  async function withdrawEther() {
+    console.log(ethereum);
+    // const provider = await alchemy.
+
+    const tx = await returnContract.withdraw({
+      from: accounts[0],
+    });
+    await tx.wait();
+    updateBalances();
+  }
+
+  // deposits .01 ETH to the faucet
+  async function depositEther() {
+    const signer = await provider.getSigner();
+
+    const tx_params = {
+      from: accounts[0],
+      to: RETURN_CONTRACT_ADDRESS,
+      value: "10000000000000000",
+    };
+
+    const tx = await signer.sendTransaction(tx_params);
+    await tx.wait();
+    updateBalances();
+  }
+
   return (
     <div className="container">
       <img className="header-image" alt="" src="https://4033360328-files.gitbook.io/~/files/v0/b/gitbook-x-prod.appspot.com/o/spaces%2FCn5NyIfWAJDviT4SySgQ%2Fuploads%2FioJYt2OT0YM59pv5WasM%2FScreenshot%202023-01-10%20at%2022.26.10.png?alt=media&amp;token=142c82ac-855c-4fbf-8a07-e7de37d9eade"></img>
@@ -102,6 +182,26 @@ function App() {
           This app allows you to simluate deposit and withdraw of 10 USDC from Circle Wallet to Return Finance smart contract.
         </span>
       </h1>
+      <div className="wallet-container">
+        <div className="wallet wallet-background">
+          <div className="balance-container">
+            <h1 className="address-item">My Wallet</h1>
+            <p className="balance-item">
+              <b>Address:</b> {accounts[0]}
+            </p>
+            <p className="balance-item">
+              <b>Balance:</b> {walletBalance} Ξ
+            </p>
+            <button
+              className="button"
+              onClick={onClickConnect}
+              disabled={buttonDisabled}
+            >
+              {buttonText}
+            </button>
+          </div>
+        </div>
+      </div>
       <div className="balance-container">
         <h2 className="balance-item">Circle Balance: 10k+ USDC</h2>
         <h2 className="balance-item">My Wallet Balance: {walletBalance} USDC</h2>
