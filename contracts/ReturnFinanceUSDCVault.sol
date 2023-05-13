ac1295ceca0d0ea266bdc146d1fd0b16af466dbd
peev <christo.peev@gmail.com>
2023-05-14 00:09:30 +0300

    Add AAVE Pool and yEarn Valut interfaces

--- contracts/ReturnFinanceUSDCVault.sol
+++ contracts/ReturnFinanceUSDCVault.sol
@@ -7,6 +7,8 @@ import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {ERC4626} from "@openzeppelin/contracts/token/ERC20/extensions/ERC4626.sol";
 
+import {IAaveV3Pool} from "./IAaveV3Pool.sol";
+import {IYearnVault} from "./IYearnVault.sol";
 
 contract ReturnFinanceUSDCVault is ERC4626, Ownable, Pausable {
     mapping(address => bool) public whitelist;
@@ -18,7 +20,18 @@ contract ReturnFinanceUSDCVault is ERC4626, Ownable, Pausable {
     uint256 public yearnPoolWeightBps = 5000;
 
     address public usdcAddress = 0x7F5c764cBc14f9669B88837ca1490cCa17c31607;
+    address public aaveV3Pool = 0x794a61358D6845594F94dc1DB02A252b5b4814aD;
+    address public aOptUSDC = 0x625E7708f30cA75bfd92586e17077590C60eb4cD;
+    address public usdcYVault = 0xaD17A225074191d5c8a37B50FdA1AE278a2EE6A2;
 
+    error NotInWhitelist(address wrongAddress);
+    error UnableToWithdraw(address token);
+    error DepositFailed(address depositor, uint256 amount);
+    error WithdrawFailed(address depositor, uint256 amount);
+
+    modifier onlyWhitelist() {
+        if (!whitelist[_msgSender()]) revert NotInWhitelist(_msgSender());
+        _;
+    }
 
     constructor() ERC4626(IERC20(usdcAddress)) ERC20(vaultName, vaultSymbol) {}
-
