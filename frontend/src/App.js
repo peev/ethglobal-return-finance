f73e03c018b6448f7f01badfd685960f233152b3
peev <christo.peev@gmail.com>
2023-05-13 12:25:24 +0300

    Install Metamask SDK

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,7 @@ function App() {
   const [faucetBalance, setFaucetBalance] = useState(0);
   return (
     <div className="container">
+      <img className="header-image" alt="" src="https://4033360328-files.gitbook.io/~/files/v0/b/gitbook-x-prod.appspot.com/o/spaces%2FCn5NyIfWAJDviT4SySgQ%2Fuploads%2FioJYt2OT0YM59pv5WasM%2FScreenshot%202023-01-10%20at%2022.26.10.png?alt=media&amp;token=142c82ac-855c-4fbf-8a07-e7de37d9eade"></img>
       <h1 className="title">
         Return Finance Demo App
         <br></br>
